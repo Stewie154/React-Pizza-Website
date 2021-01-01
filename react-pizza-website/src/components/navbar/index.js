410002c295ce20006ca4178bd914c70f1be18a10
Stewie154 <69080603+Stewie154@users.noreply.github.com>
2021-01-01 14:49:39 +0000

    Hero and Sidebar
    In this commit I've finished the Hero and Sidebar sections. I gave the Hero section some state called isOpen / setIsOpen using the useState hook.
    
    I then created a toggle function that uses 'setIsOpen' to toggle the 'isOpen' between true and false.
    
    When set to true the sidebar container 's right property is set to 0, and it's -1000px (off screen) when set to false. This is done through a ternary operator within the 'SidebarContainer' styled-component.
    
    I passed the toggle function from the Hero component down to both the Navbar and Sidebar components. I passed the isOpen state down to the Sidebar component.
    
    I gave the NavIcon and Sidebar Icon onClick props that trigger the toggle function and change the isOpen state, thus making the sidebar appear / disappear.
    
    So clicking the pizza icon opens the sidebar and clicking the cross icon on the sidebar closes it.

--- react-pizza-website/src/components/navbar/index.js
+++ react-pizza-website/src/components/navbar/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Bars, Nav, NavIcon, NavLink } from './navbarElements';
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
     return (
         <>
             <Nav>
                 <NavLink to='/'>Pizza</NavLink>
-                <NavIcon>
+                <NavIcon onClick={toggle}>
                     <p>Menu</p>
                     <Bars></Bars>
                 </NavIcon>
